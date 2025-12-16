214add5e5e5af08d70f503e503857fb3867a3af0
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2025-12-16 17:24:09 -0600

    add proof-of-concept router

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 
 const express = require('express');
+
+const defaultRouter = require('./routers/default.js');
+
 const app = express();
 const port = 8080;
 
@@ -8,6 +11,7 @@ const path = require('path');
 app.use(express.json());
 app.use(express.static(path.resolve('client', 'dist')));
 
+app.use(defaultRouter);
 
 app.listen(port, () => {
   console.info(`App available on http://localhost:${port} or http://127.0.0.1:${port}`);
