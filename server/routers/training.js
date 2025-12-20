6a987a69761ddcc1925ec8d9fa3cc61c7a83ef71
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2025-12-20 14:58:06 -0600

    update pet skill stats when training

--- server/routers/training.js
+++ server/routers/training.js
@@ -41,42 +41,48 @@ router.get('/:id', (req, res) => {
 
 // PATCH data to update the specified skill by the delta amount for the pet belonging to the current user
 router.patch('/:id', (req, res)=> {
+  // check for authentication
   const userId = req.session.passport?.user?.id;
-
   if (userId === undefined) {
     res.sendStatus(401);
     return;
   }
 
+  // get data from request
   const skillId = req.params.id;
-
   const skillDelta = req.body.delta;
 
+  // look up the pet associated with the logged in user
   Pet.findOne({userId})
     .then((pet) => {
+      // check that user has a pet
       if (!pet) {
         res.sendStatus(404);
         return;
       }
 
       // find the correct skill in the pet's training array
-      const matchingSkills = pet.training.filter((skill) => skill._id === skillId);
-      if (!matchingSkills.length) {
+      const matchingSkill = pet.training.id(skillId);
+      if (!matchingSkill) {
         res.sendStatus(404);
         return;
       }
 
       // get the current stat for this skill and add the delta from the request body
-      const newStat = matchingSkills[0].stat + skillDelta;
-      console.log(newStat);
+      const newStat = matchingSkill.stat + skillDelta;
 
-      // update the skill
-      
-      console.log(pet);
-      res.send('patch id');
+      // update the skill, capped at 100
+      pet.training[pet.training.indexOf(matchingSkill)].stat = newStat < 100 ? newStat : 100;
+
+      // parent document must be saved to save the subdocument
+      // https://mongoosejs.com/docs/8.x/docs/subdocs.html
+      return pet.save()
+        .then(() => {
+          res.sendStatus(200);
+        });
     })
     .catch((error) => {
-      console.error('Failed to find pet:', error);
+      console.error('Failed to PATCH pet skill:', error);
       res.sendStatus(500);
     });
 });
