b22b3af4217f863b6921937480835feb6d3ef81b
cfields05 <senken1218@gmail.com>
2026-01-02 12:38:27 -0600

    unfinished changes to weather route for saving weather for one location in a pretty bad way - using city instead of zip code now as well

--- server/routers/weather.js
+++ server/routers/weather.js
@@ -11,7 +11,7 @@ const options = {
   url: 'http://api.weatherapi.com/v1/current.json',
   params: {
     key: process.env['WEATHER_API_KEY'],
-    q: '70112',
+    q: 'New Orleans',
   }
 };
 
@@ -21,10 +21,36 @@ const getWeather = async () => {
 };
 
 
-router.get('/', (req, res) => {
+router.get('/api', (req, res) => {
   getWeather()
     .then(weatherInfo => {
       res.status(200).send(weatherInfo);
+      /*
+        * I think this is pretty overengineered but I'm not sure how else to do it right now
+        * gonna have a get root request to JUST get from database without doing all this... mess
+        * commented out for now because it's not finished and I need to work on it more
+
+        const { location, current: { condition, temp_f }} = weatherInfo;
+        Weather.findOne({ location: weatherInfo.location.name })
+          .then(weather => {
+            if (weather) {
+              // Weather.updateOne()
+            } else {
+              Weather.create({ location: location.name, condition: condition.text, temperature: temp_f })
+                .then(weatherDB => {
+                  res.status(200).send(weatherDB);
+                })
+                .catch(err => {
+                  console.error('Could not create weather entry: ', err);
+                  res.sendStatus(500);
+                });
+            }
+          })
+          .catch(err => {
+            console.error('Unable to find weather information in database: ', err);
+            res.sendStatus(500);
+          });
+      */
     })
     .catch(err => {
       console.error('Unable to retrieve from weather api on server: ', err);
