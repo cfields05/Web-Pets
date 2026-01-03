62e1244c6fa7d88beb579632971824a5827283d0
azaria <azariaplayer@gmail.com>
2026-01-02 21:28:46 -0600

    Documentation for ScreenView is now added

--- client/src/components/ScreenView.jsx
+++ client/src/components/ScreenView.jsx
@@ -2,6 +2,20 @@ import { useState } from 'react';
 import React from 'react';
 import axios from 'axios';
 
+
+/**
+ * @module ScreenView
+ * @description
+ * This component controls what will be displayed in the screen of the application.
+ */
+
+/**
+ * The beginning of this function shows how the screen is styled. This is what forms
+ * the rectangle box that represents the 'screen'. Using destructing to get data from
+ * deviceView.
+ * @name ScreenView
+ */
+
 const ScreenView = ({ pet, user, message , initPet}) => {
   const styles = {
     screen: [ // { border: '5px inset hotpink', height: '360px', margin: '5px', backgroundColor: 'lavender' }
@@ -14,9 +28,20 @@ const ScreenView = ({ pet, user, message , initPet}) => {
       'border-2', // border width
     ]
   };
-
+/**
+ * This is a react hook state being used to set the state of
+ * the pet name.
+ * @name nameState
+ */
   const [name, setName] = useState('');
 
+  /**
+ * The handleSubmit function sends a post request to the server
+ * when the user provides a name inside the input field when creating/adopting
+ * a pet. Note that the user is not allowed to leave the input field blank.
+ * @name handleSubmit
+ * @params {string} name - name of the pet
+ */
   const handleSubmit = () => {
     if(name === '') {
       // if the user doesn't enter anything the button does nothing
@@ -31,6 +56,13 @@ const ScreenView = ({ pet, user, message , initPet}) => {
       });
   };
 
+  /**
+ * This will render the name of the pet if the user has pet in the database.
+ * If the user does not have a pet, the input box for naming the pet will appear.
+ * If the user is not signed in and they try to create a pet, 'Please sign in'
+ * will be displayed on the page.
+ * @name renderScreenContents
+ */
   const renderScreenContents = () => {
     if (pet !== null) {
       return (
@@ -50,6 +82,12 @@ const ScreenView = ({ pet, user, message , initPet}) => {
     }
   };
 
+  /**
+ * Depending on if the user is signed in or not, the screen will display a pet or not.
+ * If the user is not signed in, not pet will show. If the user is signed in and they have no pet,
+ * or the pet ran away, no pet will display. Else, the pet will show on the screen.
+ * @name chooseImage
+ */
   const chooseImage = () => {
     //TODO: choose gif variants based on weather
     if (pet === null) {
@@ -57,9 +95,14 @@ const ScreenView = ({ pet, user, message , initPet}) => {
     } else {
       return '/sunny.gif';
     }
-  }
+  };
 
   // this is for if the user does not have a pet
+  /**
+ * This is rendering the message that is passed down through props.
+ * Also calls the functions above to render the results to the page.
+ * @name rendingScreen
+ */
   return (
     <div className={ styles.screen.join(' ') }>
       {message}
